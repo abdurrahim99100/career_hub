a66a16df18a800a3d7325f749a7ed2332b5ffc7d
cornollc <cornollc@gmail.com>
2023-04-09 23:01:37 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(<RouterProvider router={router} />)
